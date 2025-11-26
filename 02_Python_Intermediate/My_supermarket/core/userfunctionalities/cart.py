ccf8f60aa8e75fde309ae50fa13ae01ffec1026f
rishyraj18 <rishyraj18@gmail.com>
2025-11-26 16:13:53 +0530

    Minor Bugfixes

--- 02_Python_Intermediate/My_supermarket/core/userfunctionalities/cart.py
+++ 02_Python_Intermediate/My_supermarket/core/userfunctionalities/cart.py
@@ -1,27 +1,26 @@
-from core.userfunctionalities.order import order
+from core.userfunctionalities.order import order,create_order
 from core.adminfunctionalities.product import products
 
 
 class cart:
-    cart = {}
+    cart_List= {}
     def __init__(self, cart_ID):
         self._cart_ID = cart_ID
-        self._product_list = {}
 
     def add_products(self, product):
         if product in products.inventory:
-            if product not in self._product_list:           
-                self._product_list[product] = {
+            if product not in self._product_list: 
+                cart_List[cart_ID] = {
                     'name' : products.inventory[product]._product_name,
                     'price' : products.inventory[product]._product_price,
                     'quantity' : 1
                 }
-                temp = products.inventory[product]._product_name
+                temp = cart_List[cart_ID]["name"]
                 print(f"Product Added : {temp}")
             else:
-                self._product_list[product]["quantity"] += 1
-                self._product_list[product]['price'] += products.inventory[product]._product_price
-                temp = products.inventory[product]._product_name
+                cart_List[cart_ID]["quantity"] += 1
+                cart_List[cart_ID]["price"] += products.inventory[product]._product_price
+                temp = cart_List[cart_ID]["name"]
                 print(f"Product Added : {temp}")
         else:
             print("Unbale to find the Product ID")
@@ -53,35 +52,37 @@ class cart:
         if self._product_list == {}:
             print("cart is Empty")
 
-        for pid, details in self._product_list.items():
+        for cart_ID in cart_List:
             print(f"""
-Product Name: {[details["name"]]} 
-Price : {[details["price"]]}
-Quantity : {[details["quantity"]]}""")
+Product Name: {cart_List[cart_ID]["name"]} 
+Price : {cart_List[cart_ID]["price"]}
+Quantity : {cart_List[cart_ID]["quantity"]}""")
 
     def calculate_total(self):
         sub_total = 0
-        for product in self._product_list:
-            sub_total += self._product_list[product]["price"]
+        for cart_ID in cart_List:
+            quantity += cart_List[cart_ID]["price"]
 
         print(f"The Sub-total for products in cart (₹): {sub_total}")
 
     def total_quantity(self):
         quantity = 0
-        for product in self._product_list:
-            quantity += self._product_list[product]["quantity"]
+        for cart_ID in cart_List:
+            quantity += cart_List[cart_ID]["quantity"]
 
     def placeorder(self):
-        sub_total = 0
-        for product in self._product_list:
-            sub_total += self._product_list[product]["price"]
-        quantity = 0
-        for product in self._product_list:
-            quantity += self._product_list[product]["quantity"]
-        items = []
-        for product in self._product_list:
-            items.append(self._product_list[product]["name"])
-        self._cart_ID = order(self._cart_ID, sub_total, quantity, items)
+        Order_ID = self._cart_ID
+        Total_amount = 0
+        no_of_items = 0
+        Items = []
+        for cart_ID in cart_List:
+            Total_amount += cart_List[cart_ID]["price"]
+            no_of_items += cart_List[cart_ID]["quantity"]
+            items.append(cart_List[cart_ID]["Name"])
+        order.create_order(Order_ID,Total_amount, no_of_items, Items)
+          
 
 def create_cart(uname):
-    cart.cart[uname] = cart(uname)
+    cart.cart_List[uname] = cart(uname)
+
+
--- 02_Python_Intermediate/My_supermarket/core/userfunctionalities/order.py
+++ 02_Python_Intermediate/My_supermarket/core/userfunctionalities/order.py
@@ -9,10 +9,16 @@ class order:
         self._Total_amount = Total_amount
         self._no_of_items = no_of_items
         self._items = items
-        print("Order Placed, Successfully")
 
-    def display_order_details(order_id):
-        for order_id, details in order.list_of_orders.items():
-            print(f"""Order ID : {[details["Order ID"]]}
-Items = {[details["Items"]]}
-Total Amount : {[details["Total Amount"]]}""")
+    def display_order_details(order_ID):
+        for order_id in list_of_orders:
+            print(f"""
+Order ID : {list_of_orders[order_ID]._Order_ID}
+Total Amount : {list_of_orders.[order_ID]._Total_amount}
+Items : {list_of_orders[order_ID]._items}
+Total No of Items : {list_of_orders[order_ID]._no_of_items}""")
+
+def create_order(Order_ID,Total_amount, no_of_items, Items):
+    order.list_of_orders[Order_ID] = order(Order_ID, Total_amount, no_of_items,Items)
+    print("Order Placed, Successfully......")
+    display_info(Order_ID)
