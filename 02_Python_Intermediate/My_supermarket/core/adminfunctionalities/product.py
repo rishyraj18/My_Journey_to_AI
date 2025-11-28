98df5928fdfbb73acff85c3b5cbd0b64daa4a52b
rishyraj18 <rishyraj18@gmail.com>
2025-11-28 13:00:09 +0530

    changes

--- 02_Python_Intermediate/My_supermarket/core/adminfunctionalities/product.py
+++ 02_Python_Intermediate/My_supermarket/core/adminfunctionalities/product.py
@@ -6,6 +6,12 @@ class products:
         self._product_name = product_name
         self._product_price = int(product_price)
         self._product_quantity = product_quantity
+        products.inventory[product_ID] = {
+            'Product ID' : product_ID,
+            'name' : product_name,
+            'price' : product_price,
+            'quantity' : product_quantity
+        }
     
     def print_all_products():
         print("\n--------------------Products List---------------------------")
@@ -13,26 +19,26 @@ class products:
         for pid in products.inventory:
             print("Product = {i}")
             i += 1
-            print(f"Product ID : {products.inventory[pid]._product_ID}")
-            print(f"Product Name : {products.inventory[pid]._product_name}")
-            print(f"Product Price : {products.inventory[pid]._product_price}")
-            print(f"Product Quantity : {products.inventory[pid]._product_quantity}")
+            print(f"Product ID : {products.inventory[pid]['Product ID']}")
+            print(f"Product Name : {products.inventory[pid]['name']}")
+            print(f"Product Price : {products.inventory[pid]['price']}")
+            print(f"Product Quantity : {products.inventory[pid]['quantity']}")
         
     def change_product_price(self, n_price):
         if self._product_ID in products.inventory:
-            products.inventory[self._product_ID]["price"] = n_price
+            products.inventory[self._product_ID]['price'] = n_price
         else:
             print("Product details unavailable")
 
     def update_quantity(self,n_quantity):
         if self._product_ID in products.inventory:
-            products.inventory[self._product_ID]["quantity"] = n_quantity
+            products.inventory[self._product_ID]['quantity'] = n_quantity
         else:
             print("Product details unavailable")
 
-products.inventory["P1"] = products("P1", "Laptop", 50000, 5)
-products.inventory["P2"] = products("P2", "Mobile", 20000, 5)
-products.inventory["P3"] = products("P3", "Ipad", 45000, 6)
+P1 = products("P1", "Laptop", 50000, 5)
+P2 = products("P2", "Mobile", 20000, 5)
+P3 = products("P3", "Ipad", 45000, 6)
 
 def add_products():
     pid = input("Please enter product ID: ")
@@ -40,14 +46,16 @@ def add_products():
     product_price = input("Please enter product price: ")
     product_quantity = input("Please enter product quantity: ")
     products.inventory[pid] = products(pid,product_name, product_price, product_quantity)
+    print(f"Product : {product_name}, Added")
 
 def product_price():
     pid = input("Please enter product ID: ")
     product_price = input("Please enter product price: ")
     products.inventory[pid].change_product_price(product_price)
+    print("Product Price, updated")
 
 def product_quantity():
     pid = input("Please enter product ID: ")
     product_quantity = input("Please enter product quantity: ")
     products.inventory[pid].update_quantity(product_quantity)
-
+    print("Product Quantity, Updated")
